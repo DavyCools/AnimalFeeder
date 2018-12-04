13a1b41d5417cea182793ee2c6b38601e4381aa1
Hector Brasfalean <s100782@ap.be>
2018-12-04 15:25:31 +0100

    2de Activity
    Toevoegen van 2de scherm met voorraad

--- AppOfTheYear/app/src/main/java/hectorbrasfalean/ap/be/appoftheyear/MainActivity.java
+++ AppOfTheYear/app/src/main/java/hectorbrasfalean/ap/be/appoftheyear/MainActivity.java
@@ -1,13 +1,24 @@
 package hectorbrasfalean.ap.be.appoftheyear;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchCheckVoorraad(View view) {
+        Log.d(LOG_TAG,"Ga naar voorraad check");
+        Intent intent = new Intent(this, VoorraadCheck.class);
+        startActivity(intent);
     }
 }
