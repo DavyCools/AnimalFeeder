24005c2f958294030e6f7b4c9c4066fad0097b1a
Hector Brasfalean <s100782@ap.be>
2018-12-08 14:01:09 +0100

    Added RecyclerView for food

--- AppOfTheYear/app/src/main/java/hectorbrasfalean/ap/be/appoftheyear/VoorraadCheck.java
+++ AppOfTheYear/app/src/main/java/hectorbrasfalean/ap/be/appoftheyear/VoorraadCheck.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.LinkedList;
+
 public class VoorraadCheck extends AppCompatActivity {
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,20 @@ public class VoorraadCheck extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Put initial data into the word list.
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
+
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerviewFoods);
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList);
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
